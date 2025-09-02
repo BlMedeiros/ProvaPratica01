bb3116a580865598c8f295fd27dc1e183b76eb63
Bruno Luís Medeiros <165826045+BlMedeiros@users.noreply.github.com>
2025-09-02 15:55:16 -0300

    Update Main.java

--- ProvaPratica/src/Main.java
+++ ProvaPratica/src/Main.java
@@ -9,18 +9,16 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        // Criar as listas vazias para estoque
+
         List<MotorEletrico> motores = new ArrayList<>();
         List<PainelControle> paineis = new ArrayList<>();
         List<Equipamento> equipamentos = new ArrayList<>();
 
-        // Criar a interface do usuário (dependendo de como sua InterfaceUsuario funciona)
         InterfaceUsuario interfaceUsuario = new InterfaceUsuario();
 
-        // Criar o serviço passando as listas
         EstoqueService estoqueService = new EstoqueService(motores, paineis, equipamentos);
 
-        // Chamar o menu principal para iniciar o programa
         estoqueService.menuPrincipal(interfaceUsuario);
     }
 }
+
